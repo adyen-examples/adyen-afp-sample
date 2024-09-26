4cdcde2dda30d25d04bb352d43b647b826306ffc
Beppe Catanese <gcatanese@yahoo.com>
2024-09-26 10:10:08 +0200

    Helper method for StoreLocation

--- src/main/java/com/adyen/util/AddressHandler.java
+++ src/main/java/com/adyen/util/AddressHandler.java
@@ -2,10 +2,11 @@ package com.adyen.util;
 
 import com.adyen.model.StoreConfigurationAddress;
 import com.adyen.model.legalentitymanagement.Address;
+import com.adyen.model.management.StoreLocation;
 import org.springframework.stereotype.Service;
 
 /**
- * Helper class to work with the Address class
+ * Helper class to convert Address models
  */
 @Service
 public class AddressHandler {
@@ -15,7 +16,7 @@ public class AddressHandler {
      * @param address
      * @return
      */
-    public StoreConfigurationAddress getStoreConfigurationAddress(Address address) {
+    public StoreConfigurationAddress getStoreLocation(Address address) {
 
         return new StoreConfigurationAddress()
                 .city(address.getCity())
@@ -26,4 +27,19 @@ public class AddressHandler {
                 .street2(address.getStreet2());
     }
 
+    /**
+     * Create a StoreLocation from the given StoreConfigurationAddress
+     * @param address
+     * @return
+     */
+    public StoreLocation getStoreLocation(StoreConfigurationAddress address) {
+
+        return new StoreLocation()
+                .city(address.getCity())
+                .country(address.getCountry())
+                .postalCode(address.getPostalCode())
+                .stateOrProvince(address.getStateOrProvince())
+                .line1(address.getStreet())
+                .line2(address.getStreet2());
+    }
 }
