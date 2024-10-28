0ae82f9fc9ab88dff73dae51365ed325c960bf8d
Beppe Catanese <gcatanese@yahoo.com>
2024-10-28 11:12:36 +0100

    Add PayoutsOverview component

--- react-app/src/dashboard/Payouts.js
+++ react-app/src/dashboard/Payouts.js
@@ -1,14 +1,62 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Toolbar from '@mui/material/Toolbar';
 import Divider from '@mui/material/Divider';
 
+import { useNavigate } from 'react-router-dom';
+
+import Paper from '@mui/material/Paper';
+
 import DashboardHeader from "./DashboardHeader.js";
 import DashboardDrawer from "./DashboardDrawer.js";
 
+import { AdyenPlatformExperience, PayoutsOverview } from '@adyen/adyen-platform-experience-web';
+import "@adyen/adyen-platform-experience-web/adyen-platform-experience-web.css";
+
+
 export default function Payouts() {
+
+    const navigate = useNavigate()
+
+    async function sessionRequest() {
+        try {
+            const response = await fetch('/api/dashboard/getPayouts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.error("sessionRequest error: ", error);
+            throw error;
+        }
+    }
+
+    async function initializeCore() {
+        const core = await AdyenPlatformExperience({
+            environment: 'test', // test or live
+            locale: 'en-US',
+            async onSessionCreate() {
+                const session = await sessionRequest();
+                return session;
+            }
+        });
+        const payoutsOverview = new PayoutsOverview({ core: core, preferredLimit: 10, allowLimitSelection: true });
+
+        payoutsOverview.mount('#payouts-overview-container');
+    }
+
+    initializeCore();
+
   return (
     <Box sx={{ display: 'flex' }}>
 
@@ -29,6 +77,11 @@ export default function Payouts() {
         <Divider sx={{ padding: 1 }}>
             <Chip label="My Payouts" variant="elevated" sx={{ minWidth: 100, fontSize: "20px", backgroundColor: "#0abf53", color: "white" }}/>
         </Divider>
+
+        <Paper sx={{ width: '100%' }}>
+            <div id="payouts-overview-container"></div>
+        </Paper>
+
         <br/><br/>
 
       </Box>
