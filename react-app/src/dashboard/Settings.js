7216c98131aa16c1ab98ac33cb3ad7f738cdfb44
Beppe Catanese <gcatanese@yahoo.com>
2024-11-14 14:09:25 +0100

    Init component on frontend

--- react-app/src/dashboard/Settings.js
+++ react-app/src/dashboard/Settings.js
@@ -4,35 +4,96 @@ import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Toolbar from '@mui/material/Toolbar';
 import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
 
 import DashboardHeader from "./DashboardHeader.js";
 import DashboardDrawer from "./DashboardDrawer.js";
 
+import AdyenKyc from '@adyen/kyc-components';
+import '@adyen/kyc-components/styles.css';
+
+
 export default function Settings() {
 
-  return (
-    <Box sx={{ display: 'flex' }}>
+    async function sessionRequest() {
+        try {
+            const response = await fetch('/api/dashboard/getTransferInstruments', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-      <DashboardHeader/>
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
 
-      <DashboardDrawer/>
+            return await response.json();
+        } catch (error) {
+            console.error("sessionRequest error: ", error);
+            throw error;
+        }
+    }
 
-        <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              width="100%"
-              sx={{ p: 3 }}
-        >
+    async function initializeCore() {
 
-        <Toolbar />
+        const adyenKycHandler = new AdyenKyc({
+            locale: 'en-US',
+            country: 'US',
+            environment: 'test', // test or live 'https://test.adyen.com',
+            sdkToken: await sessionRequest, //token,
+            sessionRequest //getSdkToken
+          });
 
-        <Divider sx={{ padding: 1 }}>
-            <Chip label="Settings" variant="elevated" sx={{ minWidth: 100, fontSize: "20px", backgroundColor: "#0abf53", color: "white" }}/>
-        </Divider>
-        <br/><br/>
+        const legalEntityId = ""
 
-      </Box>
-    </Box>
+        const manageTransferInstrumentComponent = adyenKycHandler
+            .create('manageTransferInstrumentComponent', {
+             legalEntityId: "legalEntityId",
+             onAdd: (legalEntityId) => {console.log("onAdd")},
+             onEdit: (transferInstrumentId, legalEntityId) => { console.log("onEdit")},
+             onRemoveSuccess: (transferInstrumentId, legalEntityId) => {console.log("onRemoveSuccess")}, // Optional
+             //onInitiateRemove: (console.log("onInitiateRemove")) => {} // Optional. The minimum library version is 3.30
+            })
+            .mount('#manage-transfer-instrument-container'); // Mount to the container you created
+
+    }
+
+    initializeCore();
+
+//    function getSdkToken() {
+//      return fetch('https://api.yourcompany.com/adyen-onboarding-sdk-token',
+//            { "policy": { "roles": ["manageTransferInstrumentComponent"] } });
+//    }
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+
+          <DashboardHeader/>
+
+          <DashboardDrawer/>
+
+            <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  width="100%"
+                  sx={{ p: 3 }}
+            >
+
+                <Toolbar />
+
+                <Divider sx={{ padding: 1 }}>
+                    <Chip label="Settings" variant="elevated" sx={{ minWidth: 100, fontSize: "20px", backgroundColor: "#0abf53", color: "white" }}/>
+                </Divider>
+
+                <Paper sx={{ width: '100%' }}>
+                    <div id="manage-transfer-instrument-container"></div>
+                </Paper>
+
+                <br/><br/>
+
+            </Box>
+        </Box>
   );
 }
